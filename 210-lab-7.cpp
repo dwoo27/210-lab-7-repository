261100f3e3d9f2eda46154a9061992d73fec595f
dwoo27 <dwooed27@gmail.com>
2026-02-12 20:55:14 -0800

    10 min commit

--- 210-lab-7.cpp
+++ 210-lab-7.cpp
@@ -9,9 +9,27 @@ int main()
 {
 	const int SIZE = 5;
 	string* arr = new string[SIZE];
-	arr = ["Abel", "Ben", "Cadence", "Darren", "Elfonso"];
+	*(arr) = "Abel";
+	*(arr + 1) = "Ben";
+	*(arr + 2) = "Cadence";
+	*(arr + 3) = "Darren";
+	*(arr + 4) = "Elfonso";
+
+	cout << "Orignial array: ";
+	displayArray(arr, SIZE);
+	
 
 
 
+}
+
+string* reverseArray(string* arr, int size) {
+
+}
+
+void displayArray(string* arr, int size) {
+	for (int i = 0; i < size; i++) {
+		cout << *(arr + i) << " ";
+	}
 }
 
